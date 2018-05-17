17d7eb0a05ed9b71c7ffa7bde48f684bc666a584
Pebe <Nabscina@users.noreply.github.com>
2018-05-17 20:52:16 +0300

    osa 1 t. 3-5

--- osa1/sovellus/src/index.js
+++ osa1/sovellus/src/index.js
@@ -2,54 +2,55 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-    const kurssi = 'Half Stack -sovelluskehitys'
-    const osa1 = 'Reactin perusteet'
-    const t1 = 10
-    const osa2 = 'Tiedonvälitys propseilla'
-    const t2 = 7
-    const osa3 = 'Komponenttien tila'
-    const t3 = 14
+    const kurssi = {
+        nimi: 'Half Stack -sovelluskehitys',
+        osat: [
+            {
+                nimi: 'Reactin perusteet',
+                tehtavia: 10
+            },
+            {
+                nimi: 'Tiedonvälitys propseilla',
+                tehtavia: 7
+            },
+            {
+                nimi: 'Komponenttien tila',
+                tehtavia: 14
+            }
+        ]
+    }
 
     return (
         <div>
             <Otsikko kurssi={kurssi} />
-            <Sisalto osa1={osa1} osa2={osa2} osa3={osa3} t1={t1} t2={t2} t3={t3} />
-            <Yhteensa t1={t1} t2={t2} t3={t3} />
+            <Sisalto kurssi={kurssi} />
+            <Yhteensa kurssi={kurssi} />
         </div>
     )
 }
 
-const Otsikko = (props) => {
+const Otsikko = ({ kurssi }) => {
     return (
         <div>
-            <h1>{props.kurssi}</h1>
+            <h1>{kurssi.nimi}</h1>
         </div>
     )
 }
 
-const Sisalto = (props) => {
+const Sisalto = ({ kurssi }) => {
     return (
         <div>
-            <Osa osannimi={props.osa1} tehtava={props.t1}/>
-            <Osa osannimi={props.osa2} tehtava={props.t2}/>
-            <Osa osannimi={props.osa3} tehtava={props.t3}/>
+            <p>{kurssi.osat[0].nimi} {kurssi.osat[0].tehtavia}</p>
+            <p>{kurssi.osat[1].nimi} {kurssi.osat[1].tehtavia}</p>
+            <p>{kurssi.osat[2].nimi} {kurssi.osat[2].tehtavia}</p>
         </div>
     )
 }
 
-const Yhteensa = (props) => {
+const Yhteensa = ({ kurssi }) => {
     return (
         <div>
-            <p>yhteensä {props.t1 + props.t2 + props.t3} tehtävää</p>
-        </div>
-    )
-
-}
-
-const Osa = (props) => {
-    return (
-        <div>
-            <p>{props.osannimi} {props.tehtava}</p>
+            <p>Yhteensä {kurssi.osat[0].tehtavia + kurssi.osat[1].tehtavia + kurssi.osat[2].tehtavia} tehtävää</p>
         </div>
     )
 }
